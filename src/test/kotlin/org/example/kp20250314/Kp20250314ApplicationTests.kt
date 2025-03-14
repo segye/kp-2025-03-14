47f81a30705841aa652284c373a930f21851b37d
segye <tprpek@gmail.com>
2025-03-14 11:13:11 +0900

    initial Commit

---
+++ src/test/kotlin/org/example/kp20250314/Kp20250314ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kp20250314
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kp20250314ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
